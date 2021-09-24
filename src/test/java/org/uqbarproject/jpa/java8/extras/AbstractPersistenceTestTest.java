ce3382b35e981186609202d01dfa9dc3e24317a8
ariel- <ariel-@users.noreply.github.com>
2021-09-24 00:27:09 -0300

    Update a JUnit5

--- src/test/java/org/uqbarproject/jpa/java8/extras/AbstractPersistenceTestTest.java
+++ src/test/java/org/uqbarproject/jpa/java8/extras/AbstractPersistenceTestTest.java
@@ -1,16 +1,15 @@
 package org.uqbarproject.jpa.java8.extras;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 
 public class AbstractPersistenceTestTest extends AbstractPersistenceTest
-      implements WithGlobalEntityManager {
+    implements WithGlobalEntityManager {
 
-   @Test
-   public void transactionIsActive() {
-      assertTrue(getTransaction().isActive());
-   }
-
+  @Test
+  public void transactionIsActive() {
+    assertTrue(getTransaction().isActive());
+  }
 }
